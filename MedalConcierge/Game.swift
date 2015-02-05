2d0c05b0843f030c67519d7e18bf2ddfb09ac507
apuruni <apuruni@gmail.com>
2015-02-05 13:42:51 +0900

    simple draw action

--- MedalConcierge/Game.swift
+++ MedalConcierge/Game.swift
@@ -1,1 +1,16 @@
 import Foundation
+
+class Game {
+    var medalCollection: MedalCollection
+    var lottery: Lottery
+    init() {
+        medalCollection = MedalCollection()
+        medalCollection.createCollection()
+        
+        lottery = Lottery(collection: medalCollection)
+    }
+    
+    func drawMedal() -> Medal {
+        return lottery.draw()!
+    }
+}
--- MedalConcierge/GameScene.swift
+++ MedalConcierge/GameScene.swift
@@ -1,6 +1,11 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    let game = Game()
+    
+    let medalNameLabel = SKLabelNode(text: "NOTHING")
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         
@@ -10,9 +15,38 @@ class GameScene: SKScene {
             //medalFrame.runAction(SKAction.colorizeWithColor(UIColor.redColor(), colorBlendFactor: 1, duration: 0.5))
         }
     
+        addMedalNode()
+    }
+    
+    func addMedalNode() {
+        let medalNode = SKShapeNode(circleOfRadius: 120)
+        medalNode.fillColor = UIColor.paperColorGreen700()
+        medalNode.position = CGPoint(x: self.frame.midX, y: self.frame.height * 0.67)
+        self.addChild(medalNode)
+        
+        medalNameLabel.fontSize = 32
+        medalNameLabel.fontColor = UIColor.whiteColor()
+        medalNameLabel.position = CGPoint(x: 0, y: 0)
+        medalNode.addChild(medalNameLabel)
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        
+    }
+    override func touchesMoved(touches: NSSet, withEvent event: UIEvent) {
+        
+    }
+    override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
+        let medal = game.drawMedal()
+        println("draw a medal: \(medal)")
+        medalNameLabel.text = medal.name
+    }
+    
+    override func touchesCancelled(touches: NSSet!, withEvent event: UIEvent!) {
+        touchesEnded(touches, withEvent: event)
     }
 }
