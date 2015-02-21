1e116d0d8bf81d50864b2f882b6b0cd40653eef3
apuruni <apuruni@gmail.com>
2015-02-21 23:04:42 +0900

    create scene from code instead of sks file.

--- MedalConcierge/GameScene.swift
+++ MedalConcierge/GameScene.swift
@@ -2,7 +2,16 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    struct Constants {
+        static let backgroundQueue = dispatch_queue_create("com.mizyki-medalconcierge.backgroundQueue", DISPATCH_QUEUE_SERIAL)
+    }
+    
     let game = Game()
+    
+    // MARK: layer properties
+    let backgroundLayer = SKNode()
+    let mainLayer = SKNode()
+    let foregroundLayer = SKNode()
     
     let medalNode1 = MedalNode(circleOfRadius: 80)
     let medalNode2 = MedalNode(circleOfRadius: 80)
@@ -10,27 +19,75 @@ class GameScene: SKScene {
     var medalCollectionNode:MedalCollectionNode!
     var drawCountLabel:SKLabelNode!
     
-    override func didMoveToView(view: SKView) {
+    // MARK: Initializers
+    
+    override init(size: CGSize) {
+        super.init(size: size)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("GameScene does not support NSCoding. Use loadSceneAssetsWithCompletionHandler(_:) instead.")
+    }
+    
+    private func setup() {
+        loadBackgroundLayer()
+        loadMainLayer()
+        loadForegroundLayer()
+    }
+    
+    private func loadBackgroundLayer() {
+        addChild(backgroundLayer)
+        let backgroundSprite = SKSpriteNode(color: UIColor.paperColorPurple800(), size: size)
+        backgroundLayer.addChild(backgroundSprite)
+    }
+    
+    private func loadMainLayer() {
+        addChild(mainLayer)
         
-        let backgroundLayer = self.childNodeWithName("backgroundLayer") as! SKSpriteNode
+        let collectionContainer = SKSpriteNode(color: UIColor.paperColorPurple300(), size: CGSize(width: size.width, height: 100.0))
+        collectionContainer.position = CGPoint(x: 0, y: (size.height - collectionContainer.size.height) / 2)
+        mainLayer.addChild(collectionContainer)
         
-        let gameLayer = self.childNodeWithName("gameLayer") as! SKSpriteNode
-        
-        
-        let medalContainer = gameLayer.childNodeWithName("medalContainer") as! SKSpriteNode
-        println("medalContainer:\(medalContainer)")
-        medalContainer.color = UIColor.paperColorCyan600()
-
-        addMedalNode1(medalContainer)
-        addMedalNode2(medalContainer)
-        
-        let collectionContainer = gameLayer.childNodeWithName("collectionContainer") as! SKSpriteNode
-        println("collectionContainer:\(collectionContainer)")
         addMedalCollectionNode(collectionContainer)
-
-        drawCountLabel = gameLayer.childNodeWithName("drawCountLabel") as! SKLabelNode
         
-        self.backgroundColor = backgroundLayer.color
+        let mainContainer = SKSpriteNode(color: UIColor.paperColorPurple800(), size: CGSize(width: size.width, height: size.height - collectionContainer.size.height))
+        mainContainer.position = CGPoint(x: 0, y: -(collectionContainer.size.height) / 2)
+        mainLayer.addChild(mainContainer)
+        
+        addMedalNode1(mainContainer)
+        addMedalNode2(mainContainer)
+    }
+    
+    private func loadForegroundLayer() {
+        addChild(foregroundLayer)
+    }
+    
+    override func didChangeSize(oldSize: CGSize) {
+        println("GameScene#didChangeSize")
+        println("oldSize: \(oldSize), size: \(size)")
+        super.didChangeSize(oldSize)
+    }
+    
+    override func didMoveToView(view: SKView) {
+        println("GameScene#didMoveToView")
+        println("size: \(size)")
+
+        super.didMoveToView(view)
+        
+        setup()
+        
+//        let gameLayer = self.childNodeWithName("gameLayer") as! SKSpriteNode
+//        
+//        
+//        let medalContainer = gameLayer.childNodeWithName("medalContainer") as! SKSpriteNode
+//        println("medalContainer:\(medalContainer)")
+//        medalContainer.color = UIColor.paperColorCyan600()
+//
+//        addMedalNode1(medalContainer)
+//        addMedalNode2(medalContainer)
+//        
+//
+//        drawCountLabel = gameLayer.childNodeWithName("drawCountLabel") as! SKLabelNode
         
     }
     
@@ -57,7 +114,7 @@ class GameScene: SKScene {
     
     override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
         let medals = game.drawPackage()
-        drawCountLabel.text = "\(game.drawCount)"
+        //drawCountLabel.text = "\(game.drawCount)"
         println("draw medals: \(medals)")
         medalNode1.medal = medals[0]
         medalNode2.medal =  medals[1]
@@ -67,5 +124,16 @@ class GameScene: SKScene {
     
     override func touchesCancelled(touches: Set<NSObject>, withEvent event: UIEvent!) {
         touchesEnded(touches, withEvent: event)
+    }
+    
+    // MARK: Asset Pre-loading
+    
+    class func loadSceneAssetsWithCompletionHandler(size: CGSize, completionHandler: GameScene -> Void) {
+        dispatch_async(Constants.backgroundQueue) {
+            let loadedScene = GameScene(size: size)
+            loadedScene.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+            // loading stuff here
+            dispatch_async(dispatch_get_main_queue()) { completionHandler(loadedScene) }
+        }
     }
 }
--- MedalConcierge/GameViewController.swift
+++ MedalConcierge/GameViewController.swift
@@ -9,25 +9,9 @@
 import UIKit
 import SpriteKit
 
-extension SKNode {
-    class func unarchiveFromFile(file : String) -> SKNode? {
-        if let path = NSBundle.mainBundle().pathForResource(file, ofType: "sks") {
-            var sceneData = NSData(contentsOfFile: path, options: .DataReadingMappedIfSafe, error: nil)!
-            var archiver = NSKeyedUnarchiver(forReadingWithData: sceneData)
-            
-            archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
-            let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as! GameScene
-            archiver.finishDecoding()
-            return scene
-        } else {
-            return nil
-        }
-    }
-}
-
 class GameViewController: UIViewController {
     
-    var gameScene:GameScene?
+    var gameScene:GameScene!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,25 +26,19 @@ class GameViewController: UIViewController {
     }
     
     private func initGameScene(){
-        if let scene = GameScene.unarchiveFromFile("GameScene") as? GameScene {
-            gameScene = scene
+        
+        GameScene.loadSceneAssetsWithCompletionHandler(self.view.bounds.size) { loadedScene in
+            self.gameScene = loadedScene
             
             // Configure the view.
             let skView = self.view as! SKView
+            
+            self.gameScene.scaleMode = .AspectFill
+            
+            skView.showsDrawCount = true
             skView.showsFPS = true
-            skView.showsNodeCount = true
             
-            /* Sprite Kit applies additional optimizations to improve rendering performance */
-            skView.ignoresSiblingOrder = true
-            
-            scene.size = skView.bounds.size
-            
-            /* Set the scale mode to scale to fit the window */
-            scene.scaleMode = .AspectFit
-            
-            scene.position = CGPoint(x:0, y:0)
-            
-            skView.presentScene(scene)
+            skView.presentScene(self.gameScene)
         }
     }
 
